52a7a97be6e5bb4954e7900e4984dc4cfe2e0328
OldAgario <36843941+OldAgario@users.noreply.github.com>
2018-02-26 14:36:07 +0300

    Update oldagar.classic.user.js

--- oldagar.classic.user.js
+++ oldagar.classic.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OldAgar.PRO WHITE
 // @namespace    OldAgar.PRO
-// @version      1.4
+// @version      1.3
 // @description  OldAgar.PRO Classic Style extension
 // @author       Old One
 // @homepage     http://oldagar.pro/
@@ -24,6 +24,9 @@ GM_addStyle(`
 }
 #instructions {
 	margin: 6px 0 20px 0 !important;
+}
+.bgimg {
+	margin: 0;
 }
 .text-muted b {
 	color: #777 !important;
